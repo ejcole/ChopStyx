f54268035cb240bed14876fccdbefc9144876fb8
Matt Stone <mattstone22133@gmail.com>
2017-10-06 14:12:26 -0400

    adding file I forgot

--- Java/matt/src/algorithms/abstracts/State.java
+++ Java/matt/src/algorithms/abstracts/State.java
@@ -4,7 +4,7 @@ public abstract class State
 {
 	public abstract boolean equals(Object state);
 	public abstract int hashCode();
-	public void printState() {}
+	public abstract void printState();
 	public abstract boolean isTerminal();
 	public abstract int utility();
 	public abstract Action[] getActions();
