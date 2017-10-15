c1166280034093131e7c5f6eed42a0c86b4ab736
parkerat2 <parkerat2@appstate.edu>
2017-10-15 19:38:57 -0400

    Update createTable.py
    table 2hands

--- Python/src/createTable.py
+++ Python/src/createTable.py
@@ -1,7 +1,9 @@
 __author__ = "group"
+
 import math
-#import pandas as pd
-from AugmentedMinimax import *
+import pandas as pd
+from src.AugmentedMinimax import *
+
 
 class State:
     def __init__(self, hand=(1, 1, 1, 1), turn=0, parent=None, loop=(), key=(1, 1, 1, 1, 0), childs=(),hand_count=2):
@@ -50,27 +52,12 @@ class State:
             return -math.exp(-depth)
         else:
             return math.exp(-depth)
-# def generate_moves(state):
-#     p1 = [0, 1]
-#     p2 = [2, 3]
-#     moves = []
-#     if not state.turn:  # state[4] =0 for player 0 turn
-#         for i in p1:
-#             for j in p2:
-#                 if state.hands[i] != 0 and state.hands[j] != 0:
-#                     moves.append((i, j))
-#     else:
-#         for i in p2:
-#             for j in p1:
-#                 if state.hands[i] != 0 and state.hands[j] != 0:
-#                     moves.append((i, j))
-#     return moves
 
 
 def create_States_set(state, state_set):
     if state not in state_set:
         state_set.add(state)
-        moves = generate_moves_v(state)
+        moves = state.get_actions()
         for move in moves:
             #newState = tap(state, move) #think this was missed in the refactor?
             newState = state.result(move)
@@ -100,7 +87,6 @@ def createStates_dict(state, state_dict):
             createStates_dict(newState, state_dict)
 
 
-
 def key_maker(state):
     x = list(state.hands)
     x.append(state.turn)
@@ -115,102 +101,19 @@ def print_child(child):
             print(i)
 
 
-def minimax(state):
-    depth = 0
-    action = None
-    player = state.turn
-
-    child_states = state.childs
-    it = iter(child_states)
-    options = []
-    actions = []
-    for x in it:
-        child_state = x
-        action = next(it)
-        if(player == 0):
-            options.append(minimax_min(child_state,depth+1))
-            actions.append(action)
-        else:
-            options.append(minimax_max(child_state,depth+1))
-            actions.append(action)
-    if(player == 0):
-        return actions[options.index(max(options))]
-    else:
-        return actions[options.index(min(options))]
-
-
-def minimax_max(state, depth):
-    if(state.is_terminal()):
-        return -math.exp(depth)
-
-    v= -math.inf
-    child_states = state.childs
-    it = iter(child_states)
-    for x in it:
-        child_state = x
-        action = next(it) # action is here to move the iteration around
-        v = max(v,minimax_min(child_state,depth+1))
-    return v
-
-
-def minimax_min(state, depth):
-    if(state.is_terminal()):
-        return math.exp(depth)
-
-    v= math.inf
-    child_states = state.childs
-    it = iter(child_states)
-    for x in it:
-        child_state = x
-        action = next(it) # action is here to move the iteration around
-        v = min(v,minimax_max(child_state,depth+1))
-    return v
-
-def printHandAndAction(state, best_move_dict,state_dict):
-    print(state.key, best_move_dict[state.key])
-    it = iter(state.childs)
-    # print(len(state.childs))
-    # print(state.childs)
-
-    for iters_state in it:
-        #print(iters_state.hands)
-        child_state = iters_state
-        action = next(it) # action is here to move the iteration around
-        #print(action)
-        if action == best_move_dict[state.key]:
-            #print(action)
-            #print(child_state.key)
-            if(child_state.is_terminal()):
-                print(child_state.key)
-                if child_state.turn == 0:
-                    print("Player 1 wins")
-                else:
-                    print("Player 0 wins")
-                return
-            child_state = state_dict[child_state.key]
-            printHandAndAction(child_state, best_move_dict,state_dict)
-
-
-def generate_best_moves(state_dict):
-    best_move_dict = {}
-    for key in state_dict.keys():
-        current_key = state_dict[key].key
-        best_move_dict[current_key] = minimax(state_dict[current_key])
-    return best_move_dict
-
-
 def write_csv(state_dict):
-    best_move_dict = generate_best_moves(state_dict)
-    df = pd.DataFrame.from_dict(best_move_dict,orient='index')
-    print(df)
+    # best_move_dict = generate_best_moves(state_dict)
+    df = pd.DataFrame.from_dict(state_dict,orient='index')
+    # print(df)
     df.to_csv("Best_Moves_wrong")
 
     f = open('bestmoves.csv','w')
-    for key in best_move_dict.keys():
+    for key in state_dict.keys():
         s = str(key)
-        m = str(best_move_dict[key])
-        f.write(s[1:-1].replace(',' ,'') + "," + m[1:-1].replace(',','') +'\n')
+        m = str(state_dict[key])
+        f.write(s[1:-1].replace(',' , '') + "," + m[1:-1].replace(',','') +'\n')
     f.close()
+
 
 def play_game(state,best_moves_dict):
     while(not state.is_terminal()):
@@ -223,47 +126,148 @@ def play_game(state,best_moves_dict):
         state = (newstate.is_terminal((tapper,tapee)))
         print(state.hands)
 
+
 def main():
     # Player 1 hands, player 2 hands, player move,
     initial_state = State()
-        # initial_state.hands = (1, 1, 1, 1)
-        # initial_state.turn = 0
-        # initial_state.parent = None
-        # initial_state.loop = None
-        # initial_state.key = key_maker(initial_state.hands, initial_state.key)
-    #initial_state.hand_count = 3
-    #initial_state.hands = (1,1,1,1,1,1)
-    #initial_state.key = key_maker(initial_state)
     state_dict = {}
     createStates_dict(initial_state, state_dict)
     state_dict[initial_state.key] = initial_state
-    #best_move_dict = generate_best_moves(state_dict)
-    #write_csv(state_dict)
-    # printHandAndAction(initial_state, best_move_dict,state_dict)
-    # test = state_dict[((4,2,2,4,0))]
-    # printHandAndAction(test, best_move_dict,state_dict)
-    #
-    print("num reachable states", len(state_dict.keys()))
-
-    #print(minimax(test_state))
-    #play_game(initial_state,best_moves_dict=generate_best_moves(state_dict))
-    # initial_state.hands = (1,1,1,1,1,1)
-    # initial_state.turn=0
-    # initial_state.hand_count =3
-    # print(generate_moves_v(initial_state))
-    
+    # print("num reachable states", len(state_dict.keys()))    
     minimaxer = AugmentedMinimax()
-     
-    for state_key, state in state_dict.items():
+    bestmove = {}
+    for tups in state_dict.items():
+        state = tups[1]
+        state_key = tups[0]
         if state.turn == 0:
-            minimaxer.minimax_start_min_player(state)    
+            bestmove[state_key] = minimaxer.minimax_start_min_player(state)
         else:
-            minimaxer.minimax_start_max_player(state)    
+            bestmove[state_key] = minimaxer.minimax_start_max_player(state)
+
+    write_csv(bestmove)
 
 
 if __name__ == '__main__':
     main()
 
+# old code
+
+# main()
+    # print(minimax(test_state))
+    # play_game(initial_state,best_moves_dict=generate_best_moves(state_dict))
+    # initial_state.hands = (1,1,1,1,1,1)
+    # initial_state.turn=0
+    # initial_state.hand_count =3
+    # print(generate_moves_v(initial_state))
+    # best_move_dict = generate_best_moves(state_dict)
+    # write_csv(state_dict)
+    # printHandAndAction(initial_state, best_move_dict,state_dict)
+    # test = state_dict[((4,2,2,4,0))]
+    # printHandAndAction(test, best_move_dict,state_dict)
+    # initial_state.hands = (1, 1, 1, 1)
+    # initial_state.turn = 0
+    # initial_state.parent = None
+    # initial_state.loop = None
+    # initial_state.key = key_maker(initial_state.hands, initial_state.key)
+    # initial_state.hand_count = 3
+    # initial_state.hands = (1,1,1,1,1,1)
+    # initial_state.key = key_maker(initial_state)
+    
+# def printHandAndAction(state, best_move_dict,state_dict):
+#     print(state.key, best_move_dict[state.key])
+#     it = iter(state.childs)
+# 
+#     for iters_state in it:
+#         #print(iters_state.hands)
+#         child_state = iters_state
+#         action = next(it) # action is here to move the iteration around
+#         #print(action)
+#         if action == best_move_dict[state.key]:
+#             #print(action)
+#             #print(child_state.key)
+#             if(child_state.is_terminal()):
+#                 print(child_state.key)
+#                 if child_state.turn == 0:
+#                     print("Player 1 wins")
+#                 else:
+#                     print("Player 0 wins")
+#                 return
+#             child_state = state_dict[child_state.key]
+#             printHandAndAction(child_state, best_move_dict,state_dict)
+
+# def minimax(state):
+#     depth = 0
+#     action = None
+#     player = state.turn
+#
+#     child_states = state.childs
+#     it = iter(child_states)
+#     options = []
+#     actions = []
+#     for x in it:
+#         child_state = x
+#         action = next(it)
+#         if(player == 0):
+#             options.append(minimax_min(child_state,depth+1))
+#             actions.append(action)
+#         else:
+#             options.append(minimax_max(child_state,depth+1))
+#             actions.append(action)
+#     if(player == 0):
+#         return actions[options.index(max(options))]
+#     else:
+#         return actions[options.index(min(options))]
+
+
+# def minimax_max(state, depth):
+#     if(state.is_terminal()):
+#         return -math.exp(depth)
+#
+#     v= -math.inf
+#     child_states = state.childs
+#     it = iter(child_states)
+#     for x in it:
+#         child_state = x
+#         action = next(it) # action is here to move the iteration around
+#         v = max(v,minimax_min(child_state,depth+1))
+#     return v
+
+
+# def minimax_min(state, depth):
+#     if(state.is_terminal()):
+#         return math.exp(depth)
+#
+#     v= math.inf
+#     child_states = state.childs
+#     it = iter(child_states)
+#     for x in it:
+#         child_state = x
+#         action = next(it) # action is here to move the iteration around
+#         v = min(v,minimax_max(child_state,depth+1))
+#     return v
+
+# def generate_best_moves(state_dict):
+#     best_move_dict = {}
+#     for key in state_dict.keys():
+#         current_key = key
+#         best_move_dict[current_key] = minimax(state_dict[current_key])
+#     return best_move_dict
+
+# def generate_moves(state):
+#     p1 = [0, 1]
+#     p2 = [2, 3]
+#     moves = []
+#     if not state.turn:  # state[4] =0 for player 0 turn
+#         for i in p1:
+#             for j in p2:
+#                 if state.hands[i] != 0 and state.hands[j] != 0:
+#                     moves.append((i, j))
+#     else:
+#         for i in p2:
+#             for j in p1:
+#                 if state.hands[i] != 0 and state.hands[j] != 0:
+#                     moves.append((i, j))
+#     return moves
 
 
 
