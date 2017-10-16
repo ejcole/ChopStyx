0e84552284acf7ae496422e6b670804a2be9c86e
parkerat2 <parkerat2@appstate.edu>
2017-10-16 00:09:29 -0400

    swapped players line 132

--- Python/src/createTable.py
+++ Python/src/createTable.py
@@ -6,7 +6,7 @@ from src.AugmentedMinimax import *
 
 
 class State:
-    def __init__(self, hand=(1, 1, 1, 1), turn=0, parent=None, loop=(), key=(1, 1, 1, 1, 0), childs=(),hand_count=2):
+    def __init__(self, hand=(1, 1, 1, 1), turn=0, parent=None, loop=(), key=(1, 1, 1, 1, 0), childs=(), hand_count=2):
         self.hands = hand
         self.turn = turn #p1 or p2
         self.parent = parent
@@ -17,7 +17,7 @@ class State:
 
     def get_actions(self):
         p1 = list(range(self.hand_count))
-        p2 = list(range(self.hand_count,2*self.hand_count))
+        p2 = list(range(self.hand_count, 2 * self.hand_count))
         moves = []
         if not self.turn:  # state[4] =0 for player 0 turn
             for i in p1:
@@ -38,34 +38,24 @@ class State:
             return self
         newHands[action[1]] += newHands[action[0]]
         if newHands[action[1]] >= 5:
-            newHands[action[1]] = newHands[action[1]] % 5
+            newHands[action[1]] %= 5
         newState = State(hand=tuple(newHands), turn=(self.turn + 1) % 2, parent=self)
         newState.key = key_maker(newState)
         return newState
 
     def is_terminal(self):
         hands = list(self.hands)
-        return sum(hands[0:self.hand_count]) == 0 or sum(hands[self.hand_count:2*self.hand_count]) == 0
+        return sum(hands[0: self.hand_count]) == 0 or sum(hands[self.hand_count: 2 * self.hand_count]) == 0
 
-    def utility(self,depth):
-        if(self.turn == 0):
+    def utility(self, depth):
+        if self.turn == 0:
             return -math.exp(-depth)
         else:
             return math.exp(-depth)
 
 
-def create_States_set(state, state_set):
-    if state not in state_set:
-        state_set.add(state)
-        moves = state.get_actions()
-        for move in moves:
-            #newState = tap(state, move) #think this was missed in the refactor?
-            newState = state.result(move)
-            create_States_set(newState, state_set)
-
-
 def createStates_dict(state, state_dict):
-    #state_dict[key_maker(state)] = state
+    # state_dict[key_maker(state)] = state
     if state.key not in state_dict:
         moves = state.get_actions()
         for move in moves:
@@ -103,27 +93,27 @@ def print_child(child):
 
 def write_csv(state_dict):
     # best_move_dict = generate_best_moves(state_dict)
-    df = pd.DataFrame.from_dict(state_dict,orient='index')
+    df = pd.DataFrame.from_dict(state_dict, orient='index')
     # print(df)
     df.to_csv("Best_Moves_wrong")
 
-    f = open('bestmoves.csv','w')
+    f = open('bestmoves.csv', 'w')
     for key in state_dict.keys():
         s = str(key)
         m = str(state_dict[key])
-        f.write(s[1:-1].replace(',' , '') + "," + m[1:-1].replace(',','') +'\n')
+        f.write(s[1:-1].replace(',', '') + "," + m[1:-1].replace(',', '') + '\n')
     f.close()
 
 
-def play_game(state,best_moves_dict):
-    while(not state.is_terminal()):
+def play_game(state, best_moves_dict):
+    while not state.is_terminal():
         move = best_moves_dict[state.key]
         newstate = state.result(move)
         print(move)
         print(newstate.hands)
         tapper = int(input("Enter Tapper: "))
         tapee = int(input("Enter Tapee: "))
-        state = (newstate.is_terminal((tapper,tapee)))
+        state = (newstate.is_terminal((tapper, tapee)))
         print(state.hands)
 
 
@@ -133,13 +123,13 @@ def main():
     state_dict = {}
     createStates_dict(initial_state, state_dict)
     state_dict[initial_state.key] = initial_state
-    # print("num reachable states", len(state_dict.keys()))    
+    # print("num reachable states", len(state_dict.keys()))
     minimaxer = AugmentedMinimax()
     bestmove = {}
     for tups in state_dict.items():
         state = tups[1]
         state_key = tups[0]
-        if state.turn == 0:
+        if state.turn == 1:
             bestmove[state_key] = minimaxer.minimax_start_min_player(state)
         else:
             bestmove[state_key] = minimaxer.minimax_start_max_player(state)
@@ -172,11 +162,11 @@ if __name__ == '__main__':
     # initial_state.hand_count = 3
     # initial_state.hands = (1,1,1,1,1,1)
     # initial_state.key = key_maker(initial_state)
-    
+
 # def printHandAndAction(state, best_move_dict,state_dict):
 #     print(state.key, best_move_dict[state.key])
 #     it = iter(state.childs)
-# 
+#
 #     for iters_state in it:
 #         #print(iters_state.hands)
 #         child_state = iters_state
@@ -269,5 +259,11 @@ if __name__ == '__main__':
 #                     moves.append((i, j))
 #     return moves
 
-
-
+# def create_States_set(state, state_set):
+#     if state not in state_set:
+#         state_set.add(state)
+#         moves = state.get_actions()
+#         for move in moves:
+#             #newState = tap(state, move) #think this was missed in the refactor?
+#             newState = state.result(move)
+#             create_States_set(newState, state_set)
